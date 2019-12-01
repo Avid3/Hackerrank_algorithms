3e7c778366aee5bcff03d9786c8aa3c1170874ea
Avid3 <56166127+Avid3@users.noreply.github.com>
2019-12-01 11:29:12 -0500

    Add files via upload

---
+++ if_else_hackerrank.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 12:07:29 2019
+
+@author: Amey
+"""
+#remove specific element
+#!/bin/python
+
+
+
+
+
+n = int(input("enter number "))
+if n % 2 !=0:
+    print("Weird")
+if n<=5 and n>=2 and n%2==0:
+            print("Not Weird")
+if n<=20 and n>=6 and n%2==0:
+            print("Weird")
+            
+if n>20 and n%2==0:
+            print("Not Weird")
