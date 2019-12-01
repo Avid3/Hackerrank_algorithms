7b98cc131e2fcd59a1f07c1fb34631f74c7e8e3d
Avid3 <56166127+Avid3@users.noreply.github.com>
2019-12-01 12:27:42 -0500

    Add files via upload

---
+++ array_sum.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 12:07:29 2019
+
+@author: Amey
+"""
+#remove specific element
+#!/bin/python
+
+
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    
+    i=0
+    sum1=0
+    for i in range(0,int(len(ar))):
+            sum1=sum1+int(ar[i])
+    return(sum1)
+
+  
+
+
+
+
+    
